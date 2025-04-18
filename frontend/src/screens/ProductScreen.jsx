1d55084f9cb5acee74b557ba168b1cbd7fb780dd
Husnam ilyas <husnamilyas89@gmail.com>
2025-04-18 23:52:39 +0500

    Update the ProductScreen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Product from '../components/Product';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
 const ProductScreen = () => {
+    const {id: productId} = useParams();
+    const product = products.find((p) => p._id === productId);
+
   return (
-    <div>
-      
-    </div>
+    <>
+        <Link className='btn btn-light my-3' to='/'>
+            Go Back
+        </Link>
+
+    </>
   )
 }
 
