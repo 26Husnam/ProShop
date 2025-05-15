847b0ba726733501cc0992280976bc54b5256206
Husnam ilyas <husnamilyas89@gmail.com>
2025-05-16 01:17:04 +0500

    Worked on error handling

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { dot } from 'node:test/reporters';
+import { connect } from 'http2';
+import productRoutes from './routes/productRoutes.js';
+
+
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
+
 const app = express();
  app.get('/', (req, res) => {
   res.send('API is running!');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes); // Use product routes
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-    });
+app.use(notFound); // Middleware for 404 errors
+app.use(errorHandler); // Middleware for handling errors
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
