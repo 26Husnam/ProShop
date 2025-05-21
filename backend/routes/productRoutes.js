3bb44cce742265aea0159750d40effd266c0f7e3
Husnam ilyas <husnamilyas89@gmail.com>
2025-05-21 15:24:54 +0500

    Update Middleware for error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { getProducts, getProductById } from '../controllers/productController.js'; 
+import checkObjectId from '../middleware/checkObjectId.js';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(checkObjectId, getProductById);
 export default router;
