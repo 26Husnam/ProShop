c8628d5f187311e5db8e512e6df8c164c5eb3760
Husnam ilyas <husnamilyas89@gmail.com>
2025-07-11 01:39:18 +0500

    Worked on Checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 
@@ -14,6 +16,7 @@ function App() {
       </Container>
     </main>
     <Footer />
+    <ToastContainer/>
     </>
   )
 }
